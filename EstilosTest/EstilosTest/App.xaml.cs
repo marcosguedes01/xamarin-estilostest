40ea4ce599318b358edbc3da172185eafacd9e86
marcos.guedes <marcos.guedes@WIN-VYU8D20CMD>
2017-02-17 14:57:13 -0300

    Add project files.

---
+++ EstilosTest/EstilosTest/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace EstilosTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new MyPage();
+        }
+    }
+}
